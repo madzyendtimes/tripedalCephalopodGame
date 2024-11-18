2f8e1df39d5e55891f9c92246735236e83003af4
madcyn <76539571+MadCynWil@users.noreply.github.com>
2024-11-18 10:07:42 -0800

    added an expander enemy
    timer causes bug when scene unloaded
    really miss try catch :(
    need to add this to  enemy generation script

---
+++ area_2d.gd
@@ -0,0 +1,34 @@
+extends Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	doaniChange()
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+
+	pass
+
+func doaniChange():
+	
+	if $AnimatedSprite2D.animation=="default":
+		$AnimatedSprite2D.animation="descend"
+		$CollisionShape2D.position.y=377
+	else:
+		$CollisionShape2D.position.y=96
+		$AnimatedSprite2D.animation="default"
+	await get_tree().create_timer(2).timeout
+	if doaniChange != null:
+		doaniChange()
+
+
+func _on_area_entered(area: Area2D) -> void:
+	pass # Replace with function body.
+
+
+func _on_body_entered(body: Node2D) -> void:
+	$"../../player".hit()
+	pass # Replace with function body.
